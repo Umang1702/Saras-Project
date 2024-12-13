2e42b3dc209dfe00b35cbf3a981b734c6248afac
Umang Sharma <155832611+Umang1702@users.noreply.github.com>
2024-12-13 22:47:09 +0530

    intial commit

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "teri Meri Kahani": "https://www.youtube.com/watch?v=ZWAGn4yyRMM",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "Hanuman Chalisa": "https://www.youtube.com/watch?v=AETFvQonfV8"
+
+}
