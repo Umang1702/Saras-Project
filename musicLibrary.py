c12675a5ee97e3a596fdf5588061536792046e6e
Umang Sharma <usharma1702@gmail.com>
2024-12-22 10:58:09 +0530

    Initial Commit

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,8 +1,9 @@
 music = {
-    "teri Meri Kahani": "https://www.youtube.com/watch?v=ZWAGn4yyRMM",
-    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
-    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
-    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
-    "Hanuman Chalisa": "https://www.youtube.com/watch?v=AETFvQonfV8"
+    "perfect": "https://www.youtube.com/watch?v=kPhpHvnnn0Q",
+    "blue": "https://www.youtube.com/watch?v=GFGPSx6cPN0",
+    "baby": "https://www.youtube.com/watch?v=kffacxfA7G4",
+    "believer": "https://www.youtube.com/watch?v=W0DM5lcj6mw",
+    "weapon": "https://www.youtube.com/watch?v=5VubhvUQaD8",
+    "Hanuman":"https://www.youtube.com/watch?v=AETFvQonfV8"
 
 }
